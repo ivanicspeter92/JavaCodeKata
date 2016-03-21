cc9714e234c69c87950f7e368a5e1a4c647fa019
Peter Ivanics <peter.ivanics@reslink.fi>
2016-03-21 21:42:37 +0200

    Added function declarations in FizzBuzz.java
    isDividableByTree
    isDividableByFive
    doFizzBuzz

--- FizzBuzz/FizzBuzzCodeKata/src/FizzBuzz.java
+++ FizzBuzz/FizzBuzzCodeKata/src/FizzBuzz.java
@@ -8,5 +8,21 @@
  * @author PéterIvanics
  */
 public class FizzBuzz {
+    public boolean isDividableByTree(int number) {
+        return false;
+    }
     
+    public boolean isDividableByFive(int number) {
+        return false;
+    }
+    
+    /**
+     * Performs FizzBuzz on the given number.
+     * 
+     * @param number
+     * @return Fizz if the number is dividable by 3 and attaches Buzz if it is dividable by 5. Returns the number as a string, if the number is not dividable neither by 3 or 5.
+     */
+    public String doFizzBuzz(int number) {
+        return "";
+    }
 }
